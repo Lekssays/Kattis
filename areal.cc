506dc5e786fedd7021a8a0c6504e23350445d927
Ahmed Lekssays <Lekssays@users.noreply.github.com>
2016-04-19 12:29:44 +0100

    Create areal.cc

---
+++ areal.cc
@@ -0,0 +1,17 @@
+/* MAde with LOVE*/
+#include <iostream>
+#include <cstdio>
+#include <cmath>
+
+using namespace std;
+
+int main(void){
+    long long a;
+    double c;
+    
+    cin >> a;
+    c = sqrt(a) * 4.0;
+    printf("%.14f\n", c);
+    
+    return 0;
+}
