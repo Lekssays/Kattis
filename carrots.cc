a5a7abac60513d84ce89e1bb71282d7e7b8d9ac9
Ahmed Lekssays <Lekssays@users.noreply.github.com>
2016-04-19 12:38:38 +0100

    Create carrots.cc

---
+++ carrots.cc
@@ -0,0 +1,22 @@
+/* Made with LOVE*/
+#include <cstdio>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(void){
+    string str;
+    
+    int N, P;
+    
+    cin >> N >> P;
+    
+    for(int i = 0; i < N; i++){
+        cin >> str;
+    }
+    
+    cout << P << endl;
+    
+    return 0;
+} 
